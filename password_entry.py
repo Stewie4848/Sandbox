ddd1798d40d093eece479bbde62e0af9701fd291
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-08-22 11:39:33 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,10 @@
 """Jack Stewart"""
+password_length = 5
+valid_password = False
+while valid_password is False:
+    password = input("Password: ")
+    if len(password) >= password_length:
+        valid_password = True
+    else:
+        print("Invalid password")
+print('*' * len(password))
